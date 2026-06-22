ba61f237a9f9f3cc42cd3ab6a37dbd480fea1b40
superkush06 <superkush06@gmail.com>
2026-06-23 00:13:46 +0530

    feat: flat public API at the tinydiff package root

--- tinydiff/__init__.py
+++ tinydiff/__init__.py
@@ -1,11 +1,37 @@
 """tinydiff — reverse-mode autodiff in pure Python + NumPy."""
 
+from . import nn, ops, optim
 from .gradcheck import grad_check
 from .loss import mse_loss, softmax_crossentropy
 from .nn import Linear, Module, Sequential
-from .ops import add, div, exp, log, matmul, mean, mul, neg, pow_, relu, sigmoid, sub, sum_, tanh
+from .ops import (
+    add,
+    div,
+    exp,
+    log,
+    matmul,
+    mean,
+    mul,
+    neg,
+    pow_,
+    relu,
+    sigmoid,
+    sub,
+    sum_,
+    tanh,
+)
 from .optim import SGD, Adam
 from .tensor import Tensor
 
 __version__ = "0.1.0"
-__all__ = ["Tensor", "add", "div", "exp", "log", "matmul", "mean", "mul", "neg", "pow_", "relu", "sigmoid", "sub", "sum_", "tanh", "Linear", "Module", "Sequential", "mse_loss", "softmax_crossentropy", "SGD", "Adam", "grad_check", "__version__"]
+__all__ = [
+    "Tensor",
+    "add", "sub", "mul", "div", "matmul", "pow_", "neg",
+    "exp", "log", "relu", "sigmoid", "tanh", "sum_", "mean",
+    "Linear", "Sequential", "Module",
+    "mse_loss", "softmax_crossentropy",
+    "SGD", "Adam",
+    "grad_check",
+    "nn", "ops", "optim",
+    "__version__",
+]
